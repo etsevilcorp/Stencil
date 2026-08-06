15c1526dde3a7bf5047f4ac448d93570e90faba6
ets@etsevilcorp.com <your.email@example.com>
2026-08-06 14:50:29 +0300

    feat(structure): added perspective

--- stencil/structure.go
+++ stencil/structure.go
@@ -46,9 +46,11 @@ type Stencil struct {
 	MinHandling MinHandling `toml:"min-handling"`
 
 	// misc
-	RotateX int16 `toml:"rotate-x"` // deg clock-wise
-	RotateY int16 `toml:"rotate-y"` // a reminder that 2D images have no depth
-	RotateZ int16 `toml:"rotate-z"` // another reminder that 2D images have no depth(well, 0, actually)
+	// rotation
+	Perspective int   `toml:"perspective"`
+	RotateX     int16 `toml:"rotate-x"` // deg clock-wise
+	RotateY     int16 `toml:"rotate-y"` // a reminder that 2D images have no depth
+	RotateZ     int16 `toml:"rotate-z"` // another reminder that 2D images have no depth(well, 0, actually)
 }
 
 type Stencils map[string]Stencil
